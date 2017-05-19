f503f06d25de9ab8209f46bf1b1419091cea954c
emberger <emberger@mpp.mpg.de>
2017-05-19 10:02:29 +0200

    initial

---
+++ B4-build/B4d/CMakeFiles/exampleB4d.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4/B4d/exampleB4d.cc" "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4-build/B4d/CMakeFiles/exampleB4d.dir/exampleB4d.cc.o"
+  "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4/B4d/src/B4PrimaryGeneratorAction.cc" "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4-build/B4d/CMakeFiles/exampleB4d.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4/B4d/src/B4RunAction.cc" "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4-build/B4d/CMakeFiles/exampleB4d.dir/src/B4RunAction.cc.o"
+  "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4/B4d/src/B4dActionInitialization.cc" "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4-build/B4d/CMakeFiles/exampleB4d.dir/src/B4dActionInitialization.cc.o"
+  "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4/B4d/src/B4dDetectorConstruction.cc" "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4-build/B4d/CMakeFiles/exampleB4d.dir/src/B4dDetectorConstruction.cc.o"
+  "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4/B4d/src/B4dEventAction.cc" "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4-build/B4d/CMakeFiles/exampleB4d.dir/src/B4dEventAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/remote/pcilc7/software/Geant4/geant4.10.03.p01-install/include/Geant4"
+  "/home/iwsatlas1/emberger/Geant4/Current/SensitiveDetector/B4/B4d/include"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  "/usr/include/qt5/QtPrintSupport"
+  "/usr/include/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
